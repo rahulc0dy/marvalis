25890df80e8e36f232dd7f46bd958238b4f302bd
Rahul chakraborty <rahulchakraborty337@gmail.com>
2023-08-07 18:33:37 +0530

    apostrophies

--- app/page.js
+++ app/page.js
@@ -12,32 +12,32 @@ export default function Home() {
                 captivating realm where iconic characters leap off the pages and
                 into your imagination, where epic stories unfold with every
                 turn, and where the extraordinary becomes the norm. Dive deep
-                into the colorful tapestry of Marvel's rich history, where
+                into the colorful tapestry of Marvel&apos;s rich history, where
                 characters with incredible powers and human struggles come
                 together in a symphony of action, drama, and heroism. Uncover
                 the origins of beloved heroes like Spider-Man, Iron Man, and the
                 X-Men, as well as the enigmatic villains who challenge them at
-                every step. Marvalis isn't just a website; it's a doorway into
-                the thrilling sagas that have defined generations. Explore the
-                cosmic battles that span galaxies, the street-level heroics that
-                keep city streets safe, and the mind-bending alternate realities
-                that push the boundaries of imagination. From the cosmic majesty
-                of the Infinity Gauntlet saga to the gritty realism of
-                Daredevil's Hell's Kitchen, Marvalis has it all. Delve into
-                exclusive character profiles that delve into their strengths,
-                weaknesses, and pivotal moments. Discover the intricate web of
-                interconnected stories that make up the Marvel Universe, where
-                events in one series can have a ripple effect across the entire
-                spectrum. Immerse yourself in the artistry of the comics, from
-                the dynamic panels that capture explosive battles to the
-                emotionally charged moments that tug at your heartstrings.
-                Whether you're a seasoned Marvel veteran or a curious newcomer,
-                Marvalis offers something for everyone. Immerse yourself in the
-                tales of bravery, sacrifice, and personal growth that have
-                captivated audiences for decades. Join us on a journey through
-                the pages of Marvel history and get ready to experience the
-                extraordinary in ways you've never imagined. Your adventure
-                begins here, at Marvalis!
+                every step. Marvalis isn&apos;t just a website; it&apos;s a
+                doorway into the thrilling sagas that have defined generations.
+                Explore the cosmic battles that span galaxies, the street-level
+                heroics that keep city streets safe, and the mind-bending
+                alternate realities that push the boundaries of imagination.
+                From the cosmic majesty of the Infinity Gauntlet saga to the
+                gritty realism of Daredevil&apos;s Hell&apos;s Kitchen, Marvalis
+                has it all. Delve into exclusive character profiles that delve
+                into their strengths, weaknesses, and pivotal moments. Discover
+                the intricate web of interconnected stories that make up the
+                Marvel Universe, where events in one series can have a ripple
+                effect across the entire spectrum. Immerse yourself in the
+                artistry of the comics, from the dynamic panels that capture
+                explosive battles to the emotionally charged moments that tug at
+                your heartstrings. Whether you&apos;re a seasoned Marvel veteran
+                or a curious newcomer, Marvalis offers something for everyone.
+                Immerse yourself in the tales of bravery, sacrifice, and
+                personal growth that have captivated audiences for decades. Join
+                us on a journey through the pages of Marvel history and get
+                ready to experience the extraordinary in ways you&apos;ve never
+                imagined. Your adventure begins here, at Marvalis!
             </p>
             <div className="flex w-full gap-1 justify-evenly bg-zinc-950 p-3 ">
                 <Link
