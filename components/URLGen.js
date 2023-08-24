135cd7bc534984933f2b1b87471bb8cfca4a2481
Rahul chakraborty <rahulchakraborty337@gmail.com>
2023-08-24 13:17:43 +0530

    https urlgen

--- components/URLGen.js
+++ components/URLGen.js
@@ -5,7 +5,7 @@ const URL = (type) => {
     const private_key = process.env.NEXT_PUBLIC_MARVEL_PRIVATE_KEY;
     const timestamp = Date.now();
     const hash = md5(timestamp + private_key + public_key);
-    return `http://gateway.marvel.com/v1/public/${type}?limit=50&ts=${timestamp}&apikey=${public_key}&hash=${hash}`;
+    return `https://gateway.marvel.com/v1/public/${type}?limit=50&ts=${timestamp}&apikey=${public_key}&hash=${hash}`;
 };
 
 export default URL;
